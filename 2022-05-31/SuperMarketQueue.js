62f1d215c6b00b2a56c00d2540dcfb4c7dd47e87
Lebacon7 <101423234+Lebacon7@users.noreply.github.com>
2022-06-02 13:01:49 -0700

    SuperMarketQueue

--- 2022-05-31/SuperMarketQueue.js
+++
@@ -1,9 +0,0 @@
-function queueTime(customers, n) {
-    let arr = []
-    while(customers.length) {
-        arr.push(customers.splice(0,n));
-    }
-    console.log(arr)
-    return arr.reduce((acc,c)=>acc+c[Math.max(...arr)],0)
-}
-console.log(queueTime([2,3,10],2))
