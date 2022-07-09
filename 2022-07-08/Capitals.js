81c1e0236b7d8daf09c7e34c9fcb5d39ddf8d319
Tammy Fast <mike@lebeiko.com>
2022-07-09 02:28:39 -0500

    capitals

---
+++ 2022-07-08/Capitals.js
@@ -0,0 +1,15 @@
+/*
+Instructions
+Write a function that takes a single string (word) as argument. The function must return an ordered list containing the indexes of all capital letters in the string.
+
+Example
+Test.assertSimilar( capitals('CodEWaRs'), [0,3,4,6] );
+*/
+var capitals = function (word) {
+	let arr = word.split('')
+  let arr2 = []
+  arr.forEach((e,i)=>{
+    e===e.toUpperCase()? arr2.push(i): null;
+  })
+  return arr2
+};
